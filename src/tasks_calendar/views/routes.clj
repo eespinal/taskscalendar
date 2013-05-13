f8ee062464392a9ee68571a4bd170d4e3df8926c
Eddy Espinal <eespinal@intellisys.com.do>
2013-05-13 13:58:14 -0400

    Adding some sample routes for the tasks

--- src/tasks_calendar/views/routes.clj
+++ src/tasks_calendar/views/routes.clj
@@ -3,9 +3,12 @@
   			[noir.response :as response])
   (:use [noir.core :only [defpage]]))
 
-(defpage "/api/tasks" []
-		 (common/layout
-		 	[:p "This is an API response2"]))
+;(defpage "/api/tasks" []
+;		  (common/layout
+;	 		[:p "This is an API response2"]))
 
-(defpage "/api/tasks/json" []
+(defpage "/api/tasks" []
 		 (response/json "This is an API response2"))
+
+(defpage [:get "/api/tasks/:id"] {:keys [id]}
+		 (response/json id))
